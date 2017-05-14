3976c8022e9f7cfdfac11ab75d362e0ff366b1cf
yogeshgithub90 <yogesh.sharma.art@gmail.com>
2017-05-14 16:09:55 +0530

    Add files via upload

---
+++ src/main/java/com/eapp/controller/WelcomeController.java
@@ -0,0 +1,23 @@
+package com.eapp.controller;
+
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/yogeapp")
+public class WelcomeController {
+
+	// inject via application.properties
+	@Value("${welcome.message:test}")
+	private String message = "Hello World";
+
+	@RequestMapping("/home")
+	public String welcome(Map<String, Object> model) {
+		model.put("message", this.message);
+		return "welcome";
+	}
+
+}
